f786c0a89eab5018494093c99954e9a66a47aa9f
Threepix <98532056+Threepix@users.noreply.github.com>
2022-09-07 23:43:57 +0300

    Update 6.go

--- 6.go
+++ 6.go
@@ -4,29 +4,19 @@ import (
 	"fmt"
 	"github.com/eiannone/keyboard"
 	"math/rand"
-	"time"
+	"os"
 )
 
 func Random(min int, max int) int {
 
-	return min + rand.Intn(max-min)
+	return min + rand.Intn(max)
 }
 
 func proizv1(ch chan int) {
 	fmt.Println("post 1 started")
-	if err := keyboard.Open(); err != nil {
-		panic(err)
-	}
-	defer func() {
-		_ = keyboard.Close()
-	}()
 
 	fmt.Println("Press q to quit")
 	for true {
-		_, key, err := keyboard.GetKey()
-		if err != nil {
-			panic(err)
-		}
 		if len(ch) >= 100 {
 			fmt.Println("Continuing loop")
 			continue
@@ -34,28 +24,15 @@ func proizv1(ch chan int) {
 		if len(ch) <= 80 {
 			t := Random(1, 100)
 			ch <- t
-			fmt.Println("1 chanal make " + string(<-ch))
-		}
-		if key == keyboard.KeyCtrlQ {
-			break
+			//fmt.Println("1 chanal make " + string(<-ch))
 		}
 	}
 }
 func proizv2(ch chan int) {
 	fmt.Println("post 2 started")
-	if err := keyboard.Open(); err != nil {
-		panic(err)
-	}
-	defer func() {
-		_ = keyboard.Close()
-	}()
 
 	fmt.Println("Press q to quit")
 	for true {
-		_, key, err := keyboard.GetKey()
-		if err != nil {
-			panic(err)
-		}
 		if len(ch) >= 100 {
 			fmt.Println("Continuing loop")
 			continue
@@ -63,102 +40,150 @@ func proizv2(ch chan int) {
 		if len(ch) <= 80 {
 			t := Random(1, 100)
 			ch <- t
-			fmt.Println("2 chanal make " + string(<-ch))
-			time.Sleep(time.Second / 2)
-		}
-		if key == keyboard.KeyCtrlQ {
-			break
+			//fmt.Println("2 chanal make " + string(<-ch))
 		}
 	}
 }
-func proizv3(ch chan int) {
+func proizv3(ch chan int, dead chan bool) {
 	fmt.Println("post3 started")
-	if err := keyboard.Open(); err != nil {
-		panic(err)
-	}
-	defer func() {
-		_ = keyboard.Close()
-	}()
 	fmt.Println("Press q to quit")
+	t := Random(1, 100)
 	for true {
-		_, key, err := keyboard.GetKey()
-		if err != nil {
-			panic(err)
-		}
 		if len(ch) >= 100 {
 			fmt.Println("Continuing loop")
 			continue
 		}
-		if len(ch) <= 80 {
-			t := Random(1, 100)
-			ch <- t
-			fmt.Println("3 chanal make " + string(<-ch))
-			time.Sleep(time.Second)
-		}
-		if key == keyboard.KeyCtrlQ {
-			break
+		select {
+		case ch <- t:
+			if len(ch) <= 80 {
+				ch <- t
+				//fmt.Println("3 chanal make " + string(<-ch))
+			}
+		case <-dead:
+			close(ch)
+			return
 		}
 	}
 }
 
-func pocup1(ch1, ch2, ch3 chan int) {
+func pocup1(ch1 chan int) {
 	fmt.Println("byer 1 started")
 	for true {
-		if len(ch1) > 0 || len(ch2) > 0 || len(ch3) > 0 {
-			select {
-			case <-ch1:
-				fmt.Println(<-ch1)
-				<-ch1
-			case <-ch2:
-				fmt.Println(<-ch2)
-				<-ch2
-			case <-ch3:
-				fmt.Println(<-ch3)
-				<-ch3
-			}
+		if len(ch1) > 0 {
+			fmt.Println(<-ch1)
+			<-ch1
 		}
 		continue
 	}
 }
-func pocup2(ch1, ch2, ch3 chan int) {
-	fmt.Println("byer 2 started")
+func pocup2(ch1 chan int) {
+	fmt.Println("byer 1 started")
 	for true {
-		if len(ch1) > 0 || len(ch2) > 0 || len(ch3) > 0 {
-			select {
-			case <-ch1:
-				fmt.Println(<-ch1)
-				<-ch1
-			case <-ch2:
-				fmt.Println(<-ch2)
-				<-ch2
-			case <-ch3:
-				fmt.Println(<-ch3)
-				<-ch3
-			}
+		if len(ch1) > 0 {
+			fmt.Println(<-ch1)
+			<-ch1
 		}
 		continue
 	}
 }
 
 func main() {
-	fmt.Println("вау я запустилось")
 	chan1 := make(chan int, 200)
-	chan2 := make(chan int, 200)
-	chan3 := make(chan int, 200)
+	quit := make(chan bool)
 	fmt.Println("chanal was created")
-	//proizvoditels
 
-	go proizv1(chan1)
+	if err := keyboard.Open(); err != nil {
+		panic(err)
+	}
+	/*fmt.Println("Press q to start")
+	fmt.Println("Press q to quit")*/
 
-	go proizv2(chan2)
+	_, key, err := keyboard.GetKey()
+	fmt.Println("Press q to start")
+	if err != nil {
+		panic(err)
+	}
 
-	go proizv3(chan3)
+	go func() {
+		fmt.Println("post1 started")
+		for true {
+			t := Random(1, 100)
+			select {
+			case chan1 <- t:
+				if len(chan1) >= 100 {
+					//fmt.Println("Continuing loop")
+					continue
+				}
+				if len(chan1) <= 80 {
+					chan1 <- t
+					//fmt.Println("3 chanal make " + string(<-ch))
+				}
+			case <-quit:
+				close(chan1)
+				return
+			}
+		}
+	}()
+	go func() {
+		fmt.Println("post2 started")
+		for true {
+			t := Random(1, 100)
+			select {
+			case chan1 <- t:
+				if len(chan1) >= 100 {
+					//fmt.Println("Continuing loop")
+					continue
+				}
+				if len(chan1) <= 80 {
+					chan1 <- t
+					//fmt.Println("3 chanal make " + string(<-ch))
+				}
+			case <-quit:
+				close(chan1)
+				return
+			}
+		}
+	}()
+	go func() {
+		fmt.Println("post3 started")
+		for true {
+			t := Random(1, 100)
+			select {
+			case chan1 <- t:
+				if len(chan1) >= 100 {
+					//fmt.Println("Continuing loop")
+					continue
+				}
+				if len(chan1) <= 80 {
+					chan1 <- t
+					//fmt.Println("3 chanal make " + string(<-ch))
+				}
+			case <-quit:
+				close(chan1)
+				return
+			}
+		}
+	}()
 
 	//pokupatels
 
-	go pocup1(chan1, chan2, chan3)
+	go pocup1(chan1)
 
-	go pocup2(chan1, chan2, chan3)
+	go pocup2(chan1)
 
-	time.Sleep(15 * time.Second)
+	defer func() {
+		_ = keyboard.Close()
+	}()
+
+	go func() {
+		for {
+			if key == keyboard.KeyCtrlQ {
+				fmt.Println("в потоке щас" + string(len(chan1)))
+				go func() {
+					quit <- true
+				}()
+				os.Exit(0)
+			}
+		}
+	}()
 }
