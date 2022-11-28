19b37d999a819c3738cecb66bebcce4745476831
Threepix <98532056+Threepix@users.noreply.github.com>
2022-11-28 13:49:53 +0300

    Update 4.cpp

--- 4/4.cpp
+++ 4/4.cpp
@@ -1,12 +1,20 @@
+#include <iostream>
+#include <ctime>
+
 using namespace std;
 
 int main(){
 
     int a = 0, b = 3, c = 3;
 
+    unsigned int start_time = clock();
     for (int i = 0; i <100000000; i++){
        a += 2*b + c - i; 
     }
-
-    return 0; 
+    unsigned int end_time = clock();
+    unsigned int search_time = end_time - start_time;
+    cout << "time";
+    cout << search_time << "ms\n";
+    system("pause");
+    return 0;
 }
